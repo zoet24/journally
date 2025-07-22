314c264f1ad94adf9a31f5d5255ba4f7c85b52b3
Zoe Thexton <zoe.thexton@anya.health>
2025-07-22 21:47:46 +0100

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
